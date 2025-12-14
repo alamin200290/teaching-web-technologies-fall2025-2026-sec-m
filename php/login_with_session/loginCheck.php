429c0f8defe4532082c8a4caee37b25c0c9b8f66
alamin200290 <amin.se.aiub@gmail.com>
2025-12-14 17:08:46 +0600

    php form submission+session login

---
+++ php/login_with_session/loginCheck.php
@@ -0,0 +1,33 @@
+<?php
+    session_start();
+    //print_r($_POST);
+    //var_dump($_GET);
+    //echo 
+    //print();
+
+    if(isset($_POST['submit'])){
+
+        $username = $_REQUEST['username'];
+        $password = $_REQUEST['password'];
+
+
+        if($username == "" || $password == ""){
+            echo "null submission!";
+        }else{
+
+            if($username == $password){
+                //echo "valid user!";
+                $_SESSION['status'] = true;
+                $_SESSION['username'] = $username;
+                header('location: home.php');
+            }else{
+                echo "invalid user!";
+            }
+        }
+    }else{
+        //echo "please login first!";
+        header('location: login.php');
+    }
+
+
+?>
